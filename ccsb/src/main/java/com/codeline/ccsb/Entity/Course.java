b7121ba31e500c2c649e5ef4dd7d26f5e9cf59f8
Mulk <mulkbadar774@gmail.com>
2025-11-23 14:19:21 +0400

    Spring-Boot-task5

--- ccsb/src/main/java/com/codeline/ccsb/Entity/Course.java
+++ ccsb/src/main/java/com/codeline/ccsb/Entity/Course.java
@@ -1,12 +1,19 @@
 package com.codeline.ccsb.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Data;
 
 import java.util.Date;
 @Data
 @Builder
+@Entity
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     Integer id;
     String name;
     Integer duration;
